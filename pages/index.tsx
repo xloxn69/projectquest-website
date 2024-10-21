5286053373ea532e9814d3bdf325f0ebbec294ff
xloxn69 <bxlxja@gmail.com>
2024-10-21 06:28:24 +0000

    Added landing page

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Project Quest</h1>
+      <p>Your custom landing page content here.</p>
+    </div>
+  );
+};
+
+export default Home;
