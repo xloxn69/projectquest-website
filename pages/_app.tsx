1a7e720e6f40e823349002275f4a487275597eb7
xloxn69 <bxlxja@gmail.com>
2024-11-14 06:07:49 +0000

    test

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+import React from 'react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Component {...pageProps} />
+  );
+}
+
+export default MyApp; 
